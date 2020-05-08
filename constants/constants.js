d3dde3e2d57baf6f40f51fe318643ba4172b61e1
Scott de Jonge <dejonge.scott@gmail.com>
2020-05-08 16:35:50 +1000

    Colors

--- constants/constants.js
+++ constants/constants.js
@@ -1,15 +1,39 @@
 import palettes from './palettes.json'
 
 export const BASE_COLORS = {
-  BLUE: palettes.blue['50'].value,
-  DARK: palettes.blue['80'].value,
-  GREEN: palettes.green['50'].value,
   LIGHT: palettes.neutral['0'].value,
-  NEUTRAL: palettes.neutral['50'].value,
-  ORANGE: palettes.orange['50'].value,
-  RED: palettes.red['50'].value,
-  TURQUOISE: palettes.turquoise['50'].value,
-  YELLOW: palettes.yellow['50'].value,
+  DEFAULT: palettes.neutral['50'].value,
+  DARK: palettes.blue['80'].value,
+  BLUE: {
+    LIGHT: palettes.blue['10'].value,
+    DEFAULT: palettes.blue['50'].value,
+    DARK: palettes.blue['80'].value,
+  },
+  CYAN: {
+    LIGHT: palettes.cyan['10'].value,
+    DEFAULT: palettes.cyan['50'].value,
+    DARK: palettes.cyan['80'].value,
+  },
+  GREEN: {
+    LIGHT: palettes.green['10'].value,
+    DEFAULT: palettes.green['50'].value,
+    DARK: palettes.green['80'].value,
+  },
+  NEUTRAL: {
+    LIGHT: palettes.neutral['10'].value,
+    DEFAULT: palettes.neutral['50'].value,
+    DARK: palettes.neutral['80'].value,
+  },
+  RED: {
+    LIGHT: palettes.red['10'].value,
+    DEFAULT: palettes.red['50'].value,
+    DARK: palettes.red['80'].value,
+  },
+  YELLOW: {
+    LIGHT: palettes.yellow['10'].value,
+    DEFAULT: palettes.yellow['50'].value,
+    DARK: palettes.yellow['80'].value,
+  },
 };
 
 export const WCAG_CONTRAST_RATIO = {
