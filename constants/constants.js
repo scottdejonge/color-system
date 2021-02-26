049d762036c12ddc3968fe6b68ee9116b028c61f
Scott de Jonge <dejonge.scott@gmail.com>
2021-02-26 15:12:12 +1100

    Order colors by Hue rotation values

--- constants/constants.js
+++ constants/constants.js
@@ -6,11 +6,6 @@ export const BASE = {
 };
 
 export const SHADES = {
-  neutral: {
-    light: palettes.neutral['20'].value,
-    default: palettes.neutral['50'].value,
-    dark: palettes.neutral['80'].value,
-  },
   cyan: {
     light: palettes.cyan['20'].value,
     default: palettes.cyan['50'].value,
@@ -21,30 +16,35 @@ export const SHADES = {
     default: palettes.blue['50'].value,
     dark: palettes.blue['80'].value,
   },
-  green: {
-    light: palettes.green['20'].value,
-    default: palettes.green['50'].value,
-    dark: palettes.green['80'].value,
-  },
-  yellow: {
-    light: palettes.yellow['20'].value,
-    default: palettes.yellow['50'].value,
-    dark: palettes.yellow['80'].value,
-  },
-  red: {
-    light: palettes.red['20'].value,
-    default: palettes.red['50'].value,
-    dark: palettes.red['80'].value,
+  purple: {
+    light: palettes.purple['20'].value,
+    default: palettes.purple['50'].value,
+    dark: palettes.purple['80'].value,
   },
   magenta: {
     light: palettes.magenta['20'].value,
     default: palettes.magenta['50'].value,
     dark: palettes.magenta['80'].value,
   },
-  purple: {
-    light: palettes.purple['20'].value,
-    default: palettes.purple['50'].value,
-    dark: palettes.purple['80'].value,
+  red: {
+    light: palettes.red['20'].value,
+    default: palettes.red['50'].value,
+    dark: palettes.red['80'].value,
+  },
+  yellow: {
+    light: palettes.yellow['20'].value,
+    default: palettes.yellow['50'].value,
+    dark: palettes.yellow['80'].value,
+  },
+  green: {
+    light: palettes.green['20'].value,
+    default: palettes.green['50'].value,
+    dark: palettes.green['80'].value,
+  },
+  neutral: {
+    light: palettes.neutral['20'].value,
+    default: palettes.neutral['50'].value,
+    dark: palettes.neutral['80'].value,
   },
 };
 
