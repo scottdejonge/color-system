6437f0fbb3163267a11721495c2c9587c244a9c0
Scott de Jonge <dejonge.scott@gmail.com>
2021-05-07 16:15:37 +1000

    Add named colors

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { BASE, SHADES } from '../constants/constants'
 import Combinations from './components/Combinations.vue'
 import Palette from './components/Palette.vue'
 import Section from './components/Section.vue'
+import Swatch from './components/Swatch.vue'
 import Swatches from './components/Swatches.vue'
 
 export default {
@@ -15,6 +16,7 @@ export default {
     Combinations,
     Palette,
     Section,
+    Swatch,
     Swatches,
   },
   computed: {
@@ -36,6 +38,91 @@ export default {
     v-bind:style="style"
   >
     <h1>Color System</h1>
+    <Section
+      title="Named"
+    >
+      <h3>Base</h3>
+      <ul class="swatches">
+        <li>
+          <Swatch
+            id="neutral-90"
+            name="Base"
+            scale="90"
+            v-bind:color="$options.palettes.neutral['90'].value"
+          />
+        </li>
+      </ul>
+      <h3>Selects</h3>
+      <ul class="swatches">
+        <li>
+          <Swatch
+            id="blue-50"
+            name="Cobolt"
+            scale="50"
+            v-bind:color="$options.palettes.blue['50'].value"
+          />
+        </li>
+        <li>
+          <Swatch
+            id="blue-80"
+            name="Navy"
+            scale="80"
+            v-bind:color="$options.palettes.blue['80'].value"
+          />
+        </li>
+        <li>
+          <Swatch
+            id="green-30"
+            name="Mint"
+            scale="30"
+            v-bind:color="$options.palettes.green['30'].value"
+          />
+        </li>
+        <li>
+          <Swatch
+            id="red-10"
+            name="Pink"
+            scale="10"
+            v-bind:color="$options.palettes.red['10'].value"
+          />
+        </li>
+      </ul>
+      <h3>Indicators</h3>
+      <ul class="swatches">
+        <li>
+          <Swatch
+            id="green-50"
+            name="Positive"
+            scale="50"
+            v-bind:color="$options.palettes.green['50'].value"
+          />
+        </li>
+        <li>
+          <Swatch
+            id="red-50"
+            name="Negative"
+            scale="50"
+            v-bind:color="$options.palettes.red['50'].value"
+          />
+        </li>
+        <li>
+          <Swatch
+            id="yellow-50"
+            name="Caution"
+            scale="50"
+            v-bind:color="$options.palettes.yellow['50'].value"
+          />
+        </li>
+        <li>
+          <Swatch
+            id="purple-50"
+            name="Promote"
+            scale="50"
+            v-bind:color="$options.palettes.purple['50'].value"
+          />
+        </li>
+      </ul>
+    </Section>
     <Section
       title="Base Colors"
     >
